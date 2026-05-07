ccd61de446115afc9bc75bdd6ed8a26df8196f8e
bendsss <bendroder@gmail.com>
2026-05-06 22:02:36 -0300

    Organização

---
+++ Formulario Login/01_exemplo_login/formulario.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Login</h1>    
+<form action="..." method = "...">
+    <input type="email" name="email_usuario">
+    <label>Senha</label>
+    <input type="passowrd" name="senha_usuario"><br>
+    <button type="submit>Acessar</button>
+</form>
+</body>
+</html>
